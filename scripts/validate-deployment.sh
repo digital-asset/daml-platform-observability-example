c56017a36bc2016c2d6528482feb9283f187fc4d
Nicu Reut <nicu.reut@digitalasset.com>
2023-03-22 09:33:49 +0100

    Improve script

--- scripts/validate-deployment.sh
+++ scripts/validate-deployment.sh
@@ -7,19 +7,20 @@ set -euo pipefail
 retry=0
 result=
 until [ "$retry" -ge 5 ]; do
-    # Running the command directly in the prometheus container
-    # This is required so that we can run it in CircleCI, more details here: https://circleci.com/docs/building-docker-images/#accessing-services
-    result=$(docker exec daml_observability_prometheus curl --retry 10 --retry-connrefused 'http://prometheus:9090/api/v1/query?query=daml_health_status+%3E%3D+1' || true | grep daml_health_status )
-    if [ -z "$result" ]; then
-        echo "Services are registered healthy in prometheus."
-        echo "$result"
-        exit 0
-    else
-        echo "No healthy service yet, retrying."
-        echo "$result"
-    fi
-    retry=$((retry + 1))
-    sleep 15
+  # Running the command directly in the prometheus container
+  # This is required so that we can run it in CircleCI, more details here: https://circleci.com/docs/building-docker-images/#accessing-services
+  prometheus_result=$(docker exec daml_observability_prometheus curl --retry 10 --retry-connrefused 'http://prometheus:9090/api/v1/query?query=daml_health_status+%3E%3D+1' || true)
+  result=$($prometheus_result | grep daml_health_status)
+  if [ -z "$result" ]; then
+    echo "Services are registered healthy in prometheus."
+    echo "$result"
+    exit 0
+  else
+    echo "No healthy service yet, retrying."
+    echo "$result"
+  fi
+  retry=$((retry + 1))
+  sleep 15
 done
 
 echo "No healthy service was found in the metrics."
