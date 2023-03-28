b8601abf41f338c047c486d55368234f863e4dce
Nicu Reut <nicu.reut@digitalasset.com>
2023-03-28 07:59:15 +0200

    Index the json logs in promtail/loki [PLEN-217] (#11)

--- daml-service/entrypoint.sh
+++ daml-service/entrypoint.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 [[ -n ${VERBOSE:-} ]] && set -x
 
 # Log everything (stdout and stderr) to both log file and terminal
-exec > >(tee /var/log/promtail/"${DAML_SERVICE}") 2>&1
+exec > >(tee /var/log/promtail/"${DAML_SERVICE}".log) 2>&1
 
 eval "exec java -XX:+CrashOnOutOfMemoryError ${JAVA_OPTS:-} -jar /opt/service.jar \"\$@\""
