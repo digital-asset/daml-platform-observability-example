8736e15501a49945e0b19ad5739c917b07a392c0
hrischuk-da <curtis.hrischuk@digitalasset.com>
2023-04-12 15:23:14 -0400

    Tidy up the Readme.md; add quick start section (#14)
    Co-authored-by: Miklos <57664299+miklos-da@users.noreply.github.com>
    Co-authored-by: David Dumas <david.dumas@digitalasset.com>

--- scripts/send-json-api-requests.sh
+++ scripts/send-json-api-requests.sh
@@ -30,9 +30,11 @@ CURRENT_NUMBER=1
 while true
 do
   ((CURRENT_NUMBER+=1))
-  if [ $((CURRENT_NUMBER % 20)) -eq 0 ]
+  if [ $((CURRENT_NUMBER % 10)) -eq 0 ]
   then
     echo Iteration $CURRENT_NUMBER
+    # Reduce CPU use since it is assumed this is running on a laptop.
+    sleep 1
   fi
 
   # Generate some traffic on the standard API endpoints.
